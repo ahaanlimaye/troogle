b1194a5905d06a1f53dcde89f68d5e9c27fb9a12
ahaanlimaye <ahaan.limaye@gmail.com>
2021-04-03 14:39:37 -0400

    Inserted Comments into CLI code

--- troogle-cli/bin/index.js
+++ troogle-cli/bin/index.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
+// npm dependencies
 const axios = require('axios');
 require('yargs/yargs')(process.argv.slice(2))
+
+    // add command
     .command('add [word]', 'Add a word to the trie', {}, (argv) => {
-        axios.post(`https://troogletrie.herokuapp.com/add`, { word: argv.word })
+        axios.post(`https://troogletrie.herokuapp.com/add`, { word: argv.word }) // calls add (POST request)
             .then((res) => {
                 console.log(res.data);
             })
@@ -11,8 +14,10 @@ require('yargs/yargs')(process.argv.slice(2))
                 console.log(err);
             });
     })
+
+    // delete command
     .command('delete [word]', 'Delete a word from the trie', {}, (argv) => {
-        axios.delete(`https://troogletrie.herokuapp.com/delete`, { data: { word: argv.word } })
+        axios.delete(`https://troogletrie.herokuapp.com/delete`, { data: { word: argv.word } }) // calls delete (DELETE request)
             .then((res) => {
                 console.log(res.data);
             })
@@ -20,8 +25,10 @@ require('yargs/yargs')(process.argv.slice(2))
                 console.log(err);
             });
     })
+
+    // search command
     .command('search [word]', 'Search for a word in the trie', {}, (argv) => {
-        axios.get(`https://troogletrie.herokuapp.com/search?word=${argv.word}`)
+        axios.get(`https://troogletrie.herokuapp.com/search?word=${argv.word}`) // calls search (GET request)
             .then((res) => {
                 if (res.data) {
                     console.log(`Troogle found ${argv.word} in the trie`);
@@ -34,8 +41,10 @@ require('yargs/yargs')(process.argv.slice(2))
                 console.log(err);
             })
     })
+
+    // autocomplete command
     .command('autocomplete [word]', 'Autocomplete a word/prefix', {}, (argv) => {
-        axios.get(`https://troogletrie.herokuapp.com/autocomplete?word=${argv.word}`)
+        axios.get(`https://troogletrie.herokuapp.com/autocomplete?word=${argv.word}`) // calls autocomplete (GET request)
             .then((res) => {
                 if (res.data.length > 0) {
                     console.log(`Troogle found the following words for autocomplete:`)
@@ -51,8 +60,10 @@ require('yargs/yargs')(process.argv.slice(2))
                 console.log(err);
             })
     })
+
+    // display command
     .command('display', 'Display the trie', {}, (argv) => {
-        axios.get(`https://troogletrie.herokuapp.com/display`)
+        axios.get(`https://troogletrie.herokuapp.com/display`) // calls display (GET request)
             .then((res) => {
                 console.log(res.data);
             })
@@ -60,7 +71,11 @@ require('yargs/yargs')(process.argv.slice(2))
                 console.log(err);
             })
     })
+
+    // requires that a command be used
     .demandCommand(1, 'You need to use a command')
+
+    // configures help option (-h or --help)
     .help()
     .usage('troogle <command>')
     .scriptName('troogle')
