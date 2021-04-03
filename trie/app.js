3602c058236e24450a20d3f8f928d27a27266927
ahaanlimaye <ahaan.limaye@gmail.com>
2021-04-03 10:40:50 -0400

    changed PORT value to resolve errors

--- trie/app.js
+++ trie/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 let trie = null;
 
